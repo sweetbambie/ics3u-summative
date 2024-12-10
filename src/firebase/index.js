ef35ab3c63604aa9a5258646d6c7a87ded39df17
Sophia Miao <sophia@miao.fm>
2024-12-10 13:54:16 -0500

    firebase added

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,8 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCsSpAFbxIzRLZnCMR3nG9R9wvK-NOZI4M",
+    authDomain: "summative-30fd8.firebaseapp.com",
+    projectId: "summative-30fd8",
+    storageBucket: "summative-30fd8.firebasestorage.app",
+    messagingSenderId: "174083605033",
+    appId: "1:174083605033:web:38c01e442fc24316f35238"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
