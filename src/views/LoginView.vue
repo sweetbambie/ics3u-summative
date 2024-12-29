513695b877ac3fb3cf3c26a37e08a2eba171fb9b
Sophia Miao <sophia@miao.fm>
2024-12-28 22:51:48 -0500

    fixed the login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,33 @@
-<!-- <script setup>
-import { useRouter } from 'vue-router';
+<script setup>
+import { RouterLink, useRouter } from 'vue-router';
 import { ref } from 'vue';
-import { useRegistrationStore } from '../store';
-import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+import { useStore } from "../store"
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../firebase"
 
-const store = useRegistrationStore();
+const store = useStore();
+const router = useRouter();
 const email = ref('');
 const password = ref('');
-const router = useRouter();
 
-const handleLogin = () => {
-  if (email.value === store.email && password.value === store.password) {
+const loginByEmail = async () => {
+  try {
+    const user = (await signInWithEmailAndPassword(auth, email.value, password.value)).user;
+    store.user = user;
+    router.push("/movies/all");
+  } catch (error) {
+    console.log(error);
+    alert("There was an error signing in with email!");
+  }
+};
+
+const loginByGoogle = async () => {
+  try {
+    const user = (await signInWithPopup(auth, new GoogleAuthProvider())).user;
+    store.user = user;
     router.push("/movies");
-  } else {
-    alert('Login incorrect :(');
+  } catch (error) {
+    alert("There was an error signing in with Google!");
   }
 };
 </script>
@@ -25,11 +38,12 @@ const handleLogin = () => {
     <div class="overlay">
       <div class="form-container">
         <h2>Login to Your Account</h2>
-        <form @submit.prevent="handleLogin">
+        <form @submit.prevent="loginByEmail">
           <input v-model="email" type="email" placeholder="Email" class="input-field" required />
           <input v-model="password" type="password" placeholder="Password" class="input-field" required />
           <button type="submit" class="button login">Login</button>
         </form>
+      <button @click="loginByGoogle()" type="submit" class="button login">Login by Google</button>
       </div>
     </div>
   </div>
@@ -57,7 +71,4 @@ const handleLogin = () => {
   color: white;
   padding: 20px;
 }
-</style> -->
-
-<script></script>
-<template></template>
+</style>
