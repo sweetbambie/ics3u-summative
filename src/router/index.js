c7a5c87735ae682bc6f4912b57dc3e5e02aa1d3e
Sophia Miao <sophia@miao.fm>
2025-01-17 21:25:36 -0500

    added errorpage

--- src/router/index.js
+++ src/router/index.js
@@ -3,23 +3,38 @@ import HomeView from '../views/HomeView.vue';
 import RegisterView from '../views/RegisterView.vue';
 import LoginView from '../views/LoginView.vue';
 import MoviesView from '../views/MoviesView.vue';
-import DetailsView from '../views/DetailsView.vue';
+import DetailView from '../views/DetailsView.vue';
 import CartView from '../views/CartView.vue';
-import SettingsView from '../views/SettingsView.vue';
+import SettingView from '../views/SettingsView.vue';
+import ErrorView from '../views/ErrorView.vue';
+import { userAuthorized, useStore } from '../store';
 
 const routes = [
-    { path: '/', component: HomeView },
-    { path: '/register', component: RegisterView },
-    { path: '/login', component: LoginView },
-    { path: '/movies', component: MoviesView },
-    { path: '/movies/:id', component: DetailsView },
-    { path: '/cart', component: CartView },
-    { path: '/setting', component: SettingsView }
+  { path: '/', meta: { auth: false }, component: HomeView },
+  { path: '/register', meta: { auth: false }, component: RegisterView },
+  { path: '/login', meta: { auth: false }, component: LoginView },
+  { path: '/movies', meta: { auth: true }, component: MoviesView },
+  { path: '/movies/:id', meta: { auth: true }, component: DetailView },
+  { path: '/cart', meta: { auth: true }, component: CartView },
+  { path: '/setting', meta: { auth: false }, component: SettingView},
+  { path: '/:pathMatch(.*)*', meta: { auth: false }, component: ErrorView, },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+router.beforeEach((to, from, next) => {
+  userAuthorized.then(() => {
+    const store = useStore();
+
+    if (!store.user && to.meta.auth) {
+      next("/login");
+    } else {
+      next();
+    }
+  });
+});
 
 export default router;
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <h1>This page does not exist</h1>
+</template>
+
+<style scope>
+</style>
